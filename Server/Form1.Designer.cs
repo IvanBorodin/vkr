ba00c38202089a39ee32ebe76e3aa8a710d64aeb
IvanBorodin <51119049+IvanBorodin@users.noreply.github.com>
2019-06-13 19:33:36 +0300

    Add files via upload

---
+++ Server/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace Server
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.logBox = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(868, 106);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(51, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "0.0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(822, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Крен";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(801, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(128, 88);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "GPS";
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.White;
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox3.Location = new System.Drawing.Point(67, 60);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(51, 22);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.Text = "0.0";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.White;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(67, 25);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(51, 22);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "0.0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Долгота";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Широта";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(9, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(786, 259);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // logBox
+            // 
+            this.logBox.BackColor = System.Drawing.Color.White;
+            this.logBox.ForeColor = System.Drawing.Color.Black;
+            this.logBox.Location = new System.Drawing.Point(11, 277);
+            this.logBox.Name = "logBox";
+            this.logBox.Size = new System.Drawing.Size(918, 156);
+            this.logBox.TabIndex = 4;
+            this.logBox.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(804, 178);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 37);
+            this.button1.TabIndex = 55;
+            this.button1.Text = "Подключить систему";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(801, 251);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 13);
+            this.label4.TabIndex = 54;
+            this.label4.Text = "Port:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(801, 224);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(20, 13);
+            this.label5.TabIndex = 53;
+            this.label5.Text = "IP:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(836, 247);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(57, 20);
+            this.textBox4.TabIndex = 52;
+            this.textBox4.Text = "20501";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(827, 221);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(92, 20);
+            this.textBox5.TabIndex = 51;
+            this.textBox5.Text = "192.168.1.70";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(847, 130);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(49, 42);
+            this.pictureBox2.TabIndex = 56;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(941, 438);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.logBox);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "УСАВП";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        public System.Windows.Forms.RichTextBox logBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Server/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Server
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+         pictureBox2.Image = Image.FromFile("t.bmp");
+        }
+        public void log(String s, System.Drawing.Color col)
+        {
+            this.logDelegate = new Form1.LogDelegate(this.delegatelog);
+
+            this.logBox.Invoke(this.logDelegate, this.logBox, s, col);
+            var strings = new string[1];
+            strings[0] = s;
+            //  File.AppendAllLines(logPath, strings);
+        }
+        public void log(string s)
+        {
+            this.logDelegate = new Form1.LogDelegate(this.delegatelog);
+            this.logBox.Invoke(this.logDelegate, this.logBox, s, Color.Black);
+
+        }
+        public void delegatelog(RichTextBox richTextBox, String s, Color col)
+        {
+            try
+            {
+                richTextBox.SelectionColor = col;
+                richTextBox.AppendText(s);
+                richTextBox.SelectionColor = Color.Black;
+                richTextBox.SelectionStart = richTextBox.Text.Length;
+                if (richTextBox.Text.Length > 20000)
+                    richTextBox.Text = richTextBox.Text.Substring(richTextBox.Text.Length - 10001, 10000);
+                //  var strings = new string[1];
+                //  strings[0] = s;
+                //   File.AppendAllLines(logPath, strings);
+            }
+            catch { }
+        }
+        //  public void picBoxRefresh() { picBox.Refresh(); }
+        public delegate void LogDelegate(RichTextBox richTextBox, string is_completed, Color col);
+        public LogDelegate logDelegate;
+        public delegate void StringDelegate(string is_completed);
+        public delegate void VoidDelegate();
+        public StringDelegate stringDelegate;
+        public VoidDelegate voidDelegate;
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            ParameterVisualizer visA = new ParameterVisualizer(pictureBox1, this, "Az", Color.Black);
+            visA.mainFontDepth = 10;
+            visA.functionDepth = 1;
+            // visA.functions.Add(new Function("Ax", Color.Red));
+            //  visA.functions.Add(new Function("Ay", Color.Green));
+            // visA.functions.Add(new Function("Az", Color.Black));
+            visA.lightsOn = true;
+
+            // получаем адреса для запуска сокета
+            IPEndPoint ipPoint = new IPEndPoint(IPAddress.Parse(textBox5.Text), Convert.ToInt32(textBox4.Text));
+
+            // создаем сокет
+            Socket listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            // связываем сокет с локальной точкой, по которой будем принимать данные
+            listenSocket.Bind(ipPoint);
+
+            // начинаем прослушивание
+            listenSocket.Listen(10);
+
+            Task listen = new Task(() =>
+            {
+                log("Ожидание подключения БИНС.. ");
+                while (true)
+                {
+                    Socket handler = listenSocket.Accept();
+                    log("БИНС подключен!");
+                    while (true)
+                    {
+                        /*  try
+                          {*/
+                        // получаем сообщение
+                        StringBuilder builder = new StringBuilder();
+                        int bytes = 0; // количество полученных байтов
+                        byte[] data = new byte[256]; // буфер для получаемых данных
+
+                        do
+                        {
+                            bytes = handler.Receive(data);
+                            builder.Append(Encoding.Default.GetString(data, 0, bytes));
+                        }
+                        while (handler.Available > 0);
+
+                        double[] p = new double[13];
+
+                        string s = builder.ToString();
+                        string[] sublines = s.Split(';');
+                        if (s[0] == ';')
+                        {
+                            visA.addPoint(Convert.ToDouble(sublines[1]));
+
+                            textBox2.Invoke(new Action(() => textBox2.Text = sublines[2]));
+                            textBox3.Invoke(new Action(() => textBox3.Text = sublines[3]));
+
+                            textBox1.Invoke(new Action(() => textBox1.Text = sublines[4]));
+                            double cx = Convert.ToDouble(sublines[4]);
+                            if ((cx > 5 & cx < 10) | (cx < -5 & cx > -10))
+                            {
+                                textBox1.Invoke(new Action(() => textBox1.BackColor = Color.Yellow));
+                            }
+                            else
+                               if (cx > 10 | cx < -10)
+                            {
+                                textBox1.Invoke(new Action(() => textBox1.BackColor = Color.Red));
+                            }
+                            else
+                                textBox1.Invoke(new Action(() => textBox1.BackColor = Color.White));
+
+
+                            visA.refresh();
+                        }
+                        else
+                        {
+                            string msg = "";
+                            if (s.Contains("Волнообразный износ"))
+                            {
+                                msg = "Волнообразный износ";
+                            }
+                            if (s.Contains("Стык с дефектом"))
+                            {
+                                msg = "Стык с дефектом";
+                            }
+                            if (s.Contains("Стык"))
+                            {
+                                msg = "Стык";
+                            }
+                            if (s.Contains("Кривизна пути"))
+                            {
+                                msg = "Кривизна пути";
+                            }
+                            Task alert = new Task(() => {
+                                pictureBox2.Invoke(new Action(() => pictureBox2.Image = Image.FromFile("red.png")));
+                                System.Threading.Thread.Sleep(3000);
+                                pictureBox2.Invoke(new Action(() => pictureBox2.Image = Image.FromFile("t.bmp")));
+                            });
+                            alert.Start();
+                            //visA.drawString(msg, Brushes.Red, 14, 50, 50);
+                            // visA.functions[0].points[visA.functions[0].points.Count - 1].mark = msg;
+                            log(s);
+                            visA.refresh();
+                        }
+                        /* }
+                         catch
+                         {
+                             //  log(socketExc.Message);
+                             handler.Shutdown(SocketShutdown.Both);
+                             handler.Close();
+                             log("Подключение разорвано");
+                             break;
+                         }*/
+                    }
+                }
+            });
+            listen.Start();
+        }
+    }
+}
